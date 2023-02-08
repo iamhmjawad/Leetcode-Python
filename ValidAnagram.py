53644620ee65a943c71c86129c6ab670809b1efb
iamhmjawad <thisishmjawad@gmail.com>
2023-02-08 20:15:55 +0500

    Feb 08/23

--- ValidAnagram.py
+++ ValidAnagram.py
@@ -1,4 +1,8 @@
+from collections import Counter
+
+
 class Solution:
+    # solution 1
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
@@ -6,6 +10,8 @@ class Solution:
         countS, countT = {}, {}
 
         for i in range(len(s)):
+            # folowing line tells us that if s[i] is not in countS,
+            # then set countS[s[i]] to 0, otherwise, add 1 to countS[s[i]]
             countS[s[i]] = countS.get(s[i], 0) + 1
             countT[t[i]] = countT.get(t[i], 0) + 1
 
@@ -13,3 +19,12 @@ class Solution:
             if countS[c] != countT.get(c, 0):
                 return False
         return True
+
+    # Solution 2    - sort and compare
+    def isAnagram2(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+
+    # Solution 3    - use Counter
+
+    def isAnagram3(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
