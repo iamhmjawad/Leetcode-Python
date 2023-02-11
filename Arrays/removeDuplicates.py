2100b999e5ae19c70151ad371ad2788a052db22f
iamhmjawad <thisishmjawad@gmail.com>
2023-02-11 20:04:51 +0500

    110223

---
+++ Arrays/removeDuplicates.py
@@ -0,0 +1,10 @@
+# 26
+
+def removeDuplicates(nums):
+    l = 1
+
+    for r in range(1, len(nums)):
+        if nums[r] != nums[r-1]:
+            nums[l] = nums[r]
+            l += 1
+    return l
